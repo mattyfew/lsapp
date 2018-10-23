a874464e1ec1899477246ef8dd579d47786b5e20
Matt Fewer <mattfewer@hotmail.com>
2018-10-23 11:29:16 +0200

    first commit, finished with vid 3

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'LSAPP') }}</title>
+    </head>
+    <body>
+        <h1>About</h1>
+
+        <p>This is the about page</p>
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'LSAPP') }}</title>
+    </head>
+    <body>
+        <h1>Welcome to Laravel</h1>
+
+        <p>This is the laravel app from the "Laravel From Scratch" Youtube series</p>
+    </body>
+</html>
