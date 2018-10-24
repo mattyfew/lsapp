38852941fc48f49a758b83a0891f56be153320c8
Matt Fewer <mattfewer@hotmail.com>
2018-10-24 13:58:37 +0200

    part 8 done

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
     <a href="/lsapp/public/posts" class="btn btn-default">Go Back</a>
-    <h1>{{ $post->title }}</h1>
+    <h1>{{$post->title}}</h1>
     <div>
-        {{!!$post->body!!}}
+        {!!$post->body!!}
     </div>
     <hr>
     <small>Written on {{ $post->created_at }}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
